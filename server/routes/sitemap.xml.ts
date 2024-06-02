0592626c7ba2f2e8aa64d84cee1b17879cfac35e
Amir Zuhdi Wibowo <cakadi190@gmail.com>
2024-06-02 16:04:36 +0700

    "Updated sitemap.xml.ts to include category and tag routes, and refactored route generation logic."

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -5,32 +5,43 @@ import { serverQueryContent } from '#content/server';
 export default defineEventHandler(async (event) => {
   const data = await serverQueryContent(event).find();
 
-	const otherRoutes = [
-		{
-			url: '/tentang',
-			changefreq: 'yearly',
-			priority: 0.5
-		},
-		{
-			url: '/tentang/kebijakan-privasi',
-			changefreq: 'yearly',
-			priority: 0.5
-		},
-		{
-			url: '/tentang/kebijakan-berkomentar',
-			changefreq: 'yearly',
-			priority: 0.5
-		},
-	];
+  const otherRoutes = [
+    { url: '/tentang', changefreq: 'yearly', priority: 0.5 },
+    { url: '/tentang/kebijakan-privasi', changefreq: 'yearly', priority: 0.5 },
+    { url: '/tentang/kebijakan-berkomentar', changefreq: 'yearly', priority: 0.5 },
+  ];
 
-  const routes = data.map(item => ({
+  // Ekstrak kategori unik dan buat rutenya
+  const categories = [...new Set(data.flatMap(item => item.category))]
+    .map(category => ({
+      url: `/kategori/${category.toLowerCase().replace(/\s+/g, '-')}`,
+      changefreq: 'daily',
+      priority: 0.8
+    }));
+
+  // Ekstrak tagar unik dan buat rutenya
+  const tags = [...new Set(data.flatMap(item => item.tags))]
+    .map(tags => ({
+      url: `/tagar/${tags.toLowerCase().replace(/\s+/g, '-')}`,
+      changefreq: 'daily',
+      priority: 0.8
+    }));
+
+  // Buat rute untuk setiap item data
+  const contentRoutes = data.map(item => ({
     url: item._path,
     changefreq: 'daily',
-    priority: 0.8
+    priority: 0.9
   }));
 
-	otherRoutes.map((item) => routes.push(item));
-  routes.unshift({ url: '/', changefreq: 'daily', priority: 1.0 });
+  // Gabungkan semua rute
+  const routes = [
+    { url: '/', changefreq: 'daily', priority: 1.0 },
+    ...contentRoutes,
+    ...otherRoutes,
+    ...categories,
+		...tags
+  ];
 
   const sitemap = generateSitemap(routes);
 
